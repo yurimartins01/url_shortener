aae0e489aa2a30f8bb18f93229cba98359e786f2
Yuri Martins <yuripedrocchi01@gmail.com>
2026-08-01 06:04:34 -0300

    feat: adiciona entidade ShortLink e configuração do EF Core

---
+++ src/UrlShortener.Api/Infrastructure/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.Api.Domain;
+
+namespace UrlShortener.Api.Infrastructure;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) { }
+    DbSet<ShortLink> ShortLinks => Set<ShortLink>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
--- src/UrlShortener.Api/Program.cs
+++ src/UrlShortener.Api/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using UrlShortener.Api.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options => options
+    .UseNpgsql(builder.Configuration.GetConnectionString("Default"))
+    .UseSnakeCaseNamingConvention()
+);
 
 var app = builder.Build();
 
@@ -11,7 +19,5 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
-app.UseHttpsRedirection();
 
 app.Run();
