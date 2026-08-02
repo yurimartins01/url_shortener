1e444bee27eb65d9af17261a4addf07f223741c4
Yuri Martins <yuripedrocchi01@gmail.com>
2026-08-02 10:19:57 -0300

    fix: adiciona arquivos do gerador que ficaram fora do commit anterior

---
+++ src/UrlShortener.Api/Domain/Base62ShortCodeGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+
+namespace UrlShortener.Api.Domain;
+
+public class Base62ShortCodeGenerator : IShortCodeGenerator
+{
+    private const string Base62Alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private const int CodeLength = 7;
+    public string Generate()
+    {
+        var character = RandomNumberGenerator.GetItems<char>(Base62Alphabet, CodeLength);
+
+        return new string(character);
+    }
+}
