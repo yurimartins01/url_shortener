b3a3e71294c21d9b1a7362c689fb5919b93098ee
yurimartins01 <yuripedrocchi01@gmail.com>
2026-08-13 19:57:26 -0300

    feat: adiciona teste de integração do fluxo criar -> redirecionar

---
+++ tests/UrlShortener.Api.Tests/Integration/ShortLinkFlowTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http.Json;
+using UrlShortener.Api.Contracts;
+
+namespace UrlShortener.Api.Tests
+{
+    public class ShortLinkFlowTests : IClassFixture<CustomWebApplicationFactory>
+    {
+        private readonly HttpClient _http;
+        private readonly CreateShortLinkRequest _request = new("https://example.com");
+
+
+        public ShortLinkFlowTests(CustomWebApplicationFactory factory)
+        {
+            _http = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false,
+            });
+        }
+
+        [Fact]
+        public async Task POST_WhenValidURL_ReturnsStatusCode201()
+        {
+
+            var response = await _http.PostAsJsonAsync("/api/short-url", _request);
+
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+
+        }
+
+        [Fact]
+        public async Task GET_WhenCodeExists_ReturnsStatusCode302()
+        {
+
+            var postResponse = await _http.PostAsJsonAsync("/api/short-url", _request);
+
+            Assert.Equal(HttpStatusCode.Created, postResponse.StatusCode);
+
+            var result = await postResponse.Content.ReadFromJsonAsync<CreateShortLinkResponse>();
+
+
+            var getResponse = await _http.GetAsync($"/{result!.Code}");
+
+            Uri originalUrl = new(_request.OriginalUrl);
+
+            Assert.Equal(HttpStatusCode.Found, getResponse.StatusCode);
+            Assert.Equal(originalUrl, getResponse.Headers.Location);
+
+        }
+
+    }
+}
